8cc7539ca7755467be5b7330cbcb5bb2ff457f8d
Diego Maia <diego.maia83@gmail.com>
2022-08-08 17:09:52 -0300

    Modelo de envio de Email

---
+++ MailSender/Controllers/MailController.cs
@@ -0,0 +1,31 @@
+﻿using MailSender.Models;
+using System;
+using System.Web.Mvc;
+
+namespace MailSender.Controllers
+{
+    public class MailController : Controller
+    {
+        public ActionResult Send()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Send(Contato contato)
+        {
+            try
+            {
+                Mail.EnviarEmail(contato, Request.ServerVariables["REMOTE_ADDR"]);
+
+                ViewBag.Message = "Email enviado!";
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = "Erro ao enviar o e-mail ( " + ex.Message + " )";
+            }
+
+            return View();
+        }
+    }
+}
---
+++ MailSender/Models/Mail.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Configuration;
+
+namespace MailSender.Models
+{
+    public class Contato
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Mensagem { get; set; }
+    }
+
+    public class Mail
+    {
+        public static void EnviarEmail(Contato contato, string ip)
+        {
+            var body = "<html>" +
+               "<body>" +
+               "<h3>Contato do Site - " + ConfigurationManager.AppSettings["SiteName"] + "</h3>" +
+               "<p><b>Nome:</b> " + contato.Nome + "</p>" +
+               "<p><b>E-mail:</b> " + contato.Email + "</p>" +
+               "<p><b>Mensagem:</b> <br/>" + contato.Mensagem + " </p>" +
+               "<p>&nbsp;</p>" +
+               "<p>&nbsp;</p>" +
+               "<p>Atenciosamente, <br/></p>" +
+               "<p>" + ConfigurationManager.AppSettings["SiteName"] + "</p>" +               
+               "<p>Origem da operação: " + ip + " " + DateTime.Now + "</p>" +
+               "</body>" +
+               "</html>";
+            string[] anexos = { };
+
+            Mailer.Send(contato.Email, contato.Email, "Contato do Site", body, "", new string[0]);
+
+        }
+    }
+}
